be6e5cc899f280d8dd7a6f9b468a9a1358f88c41
AnochAno <anothankes876@gmail.com>
2024-02-14 23:31:28 +0530

    Header&footer Components added

---
+++ Front-End_Ems/ui/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span>All rights reserved 2024 by <b>Anojan.T</b></span>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
--- Front-End_Ems/ui/src/Services/employedservices.jsx
+++ Front-End_Ems/ui/src/Services/employedservices.jsx
@@ -1,3 +1,7 @@
 import axios from 'axios'
 
 const REST_API_Common_URL = 'http://localhost:8080/api/employes'
+
+export const listempoyes = () => {
+    return axios.get(REST_API_Common_URL);
+}
