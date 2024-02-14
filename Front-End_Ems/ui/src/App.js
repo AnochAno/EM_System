0564317077082e55093ebc34a70cd8d94f125ab7
AnochAno <anothankes876@gmail.com>
2024-02-15 00:01:35 +0530

    routes

--- Front-End_Ems/ui/src/App.js
+++ Front-End_Ems/ui/src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Employelist from './Components/employelist'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Header/>
-    <Employelist />
-    <Footer/>
+      <Router>
+        <Header />
+          <Routes>
+            <Route path='/' element = {Employelist}></Route>
+          </Routes>
+
+        <Employelist />
+        <Footer />
+      </Router>
     </>
   );
 }
