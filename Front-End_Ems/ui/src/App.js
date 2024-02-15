95103d19b75bb1cdf162cb7ed0145862fa9a7a3f
AnochAno <anothankes876@gmail.com>
2024-02-15 12:08:14 +0530

    using route to header and footer

--- Front-End_Ems/ui/src/App.js
+++ Front-End_Ems/ui/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Headers from './Components/Header'
+import Footer from './Components/Footer'
 import Employelist from './Components/employelist';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -10,7 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Employelist />} />
         </Routes>
-       
+     
       </Router>
     </>
   );
