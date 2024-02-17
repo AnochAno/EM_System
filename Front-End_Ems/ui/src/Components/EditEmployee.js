d3407c3af892c53e200e8953b4368ffc3eb2fa7b
AnochAno <anothankes876@gmail.com>
2024-02-17 21:12:37 +0530

    Edit page Added

--- Front-End_Ems/ui/src/Components/EditEmployee.js
+++ Front-End_Ems/ui/src/Components/EditEmployee.js
@@ -1,4 +1,17 @@
+import { Container } from '@mui/material'
 import React from 'react'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 
 const EditEmployee = () => {
@@ -6,9 +19,41 @@ const EditEmployee = () => {
     
     
   return (
-    <div>
-      <h1 style={{textAlign:"center"}}>Edit Employee Details</h1>
-    </div>
+    <>
+               <div className='row'>
+        <div className='card col-md-6 offset-md-3 offset-md-3'>
+        <div>
+            <h1 style={{textAlign:"center"}}>Edit Employee Details</h1>
+        </div>
+          <div className='card-body'>
+            <form className='form-group mb-2'>
+              <label className='form-label'>First Name</label>
+              <input type='text' placeholder='Enter the First Name' name='firstname' className='form-control' />
+            </form>
+            <form className='form-group mb-2'>
+              <label className='form-label'>Last Name</label>
+              <input type='text' placeholder='Enter the last Name' name='lastname'  className='form-control'  />
+            </form>
+            <form className='form-group mb-2'>
+              <label className='form-label'>Email ID</label>
+              <input type='email' placeholder='Enter the Email ID' name='emailID'  className='form-control'  />
+            </form>
+            <form className='form-group mb-2'>
+              <label className='form-label'>Phone No</label>
+              <input type='phone' placeholder='Enter the phone Nunmber' name='phone'  className='form-control'  />
+            </form>
+            <button type="button" id="updateButton" class="btn btn-warning">Update Employee</button>
+            <button type="button" id="deleteButton" class="btn btn-danger">Delete Employee</button>
+            <button type="button" id="cancelButton" class="btn btn btn-secondary">Cancel</button>
+
+
+          </div>
+
+        </div>
+      </div>
+                   
+   
+      </>  
   )
 }
 
