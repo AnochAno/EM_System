319d0cdff02ea5de0d4ab2e4d182c8b7459088c6
AnochAno <anothankes876@gmail.com>
2024-02-18 22:16:44 +0530

    update handle

--- Front-End_Ems/ui/src/Services/employedservices.jsx
+++ Front-End_Ems/ui/src/Services/employedservices.jsx
@@ -1,7 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const REST_API_Common_URL = 'http://localhost:8080/api/employes'
+const REST_API_Common_URL = 'http://localhost:8080/api/employes';
 
 export const listempoyes = () => axios.get(REST_API_Common_URL);
 
 export const createEmploye = (addEmploye) => axios.post(REST_API_Common_URL, addEmploye);
+
+export const updateEmploye = (employeId, employeData) => axios.put(`${REST_API_Common_URL}/${employeId}`, employeData);
